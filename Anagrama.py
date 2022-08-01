e3d7fac4474419fc4fbf217ce8f487c3c9a0cdbc
AbdiCast <di.cast.0120@gmail.com>
2022-07-31 21:58:58 -0500

    Anagrama.py: agregado al repositorio

---
+++ Anagrama.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+cadena1 = input("Introduce la primera cadena: ").upper()
+cadena2 = input("Introduce la segunda cadena: ").upper()
+
+if cadena1.isspace() and cadena2.isspace():
+    print('Introduce caracteres ademas de espacios en blanco.')
+elif sorted(cadena1) == sorted(cadena2):
+
+    print("Anagrama")
+else:
+    print("No son Anagramas")
+# coding=utf-8
